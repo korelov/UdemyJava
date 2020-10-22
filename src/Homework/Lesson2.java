b94fb1eb3c50d1eae68e1d03cf4fa806451cbb02
Maksim <Korelov@gmail.com>
2020-10-22 22:47:38 +0300

    lesson2 homework done

---
+++ src/Homework/Lesson2.java
@@ -0,0 +1,66 @@
+package Homework;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        byte b1 = 0b1100;
+        byte b2 = 014;
+        byte b3 = 12;
+        byte b4 = 0xc;
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        short s1 = 0b10100010100;
+        short s2 = 0_2424;
+        short s3 = 1300;
+        short s4 = 0x514;
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0b0;
+        int i2 = 00;
+        int i3 = 0;
+        int i4 = 0x0;
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        long l1 = 0b111010110111100110100010101l;
+        long l2 = 0726746425;
+        long l3 = 123456789;
+        long l4 = 0x75bcd15l;
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        float f1 = 3.14f;
+        float f2 = 32.15f;
+        System.out.println(f1);
+        System.out.println(f2);
+
+        double d1 = 3.14;
+        double d2 = 32.15;
+        System.out.println(d1);
+        System.out.println(d2);
+
+        boolean bo1 = false;
+        boolean bo2 = true;
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+        char c1 = 500;
+        char c2 = 'a';
+        char c3 = '\u0555';
+
+        System.out.println(c3);
+        System.out.println(c2);
+        System.out.println(c1);
+
+
+    }
+}
--- src/Lesson2/DataType.java
+++ src/Lesson2/DataType.java
@@ -1,4 +1,47 @@
 package Lesson2;
 
+/**
+ * byte 8 bit -128 127
+ * short 16 bit -32768 32767
+ * int 32 bit - 2147483648  2147483647    defaul
+ * long 64 bit -9223372036854775808 9223372036854775807
+ *
+ * float 32 bit  3.4e-38 3.4e+38
+ * double 64 bit 1.7e-308 1.7e+308  defaul
+ *
+ * char 16bit 0 65535
+ * boolean  true false
+ */
+
 public class DataType {
+    public static void main(String[] args) {
+
+        //тип переменной  имя переменной = значение;
+
+        byte b1 = 21;
+        short s1 = -435;
+        int b3 = 23_333;
+        long l1 = 1000;
+        long l2 = 3_333_333_333L; // пишим с буквой L или l т.к. по умолчанию считает как int
+        float f1 = 3.14F;
+        float f2 = 3.14f; // без F или f будут восприниматься как double
+        double d1 = 3.14;
+        char c1 = 'a'; //символ в одинарных ковычках и только один, пробел это тоже символ
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        //Система счислений
+
+        int a1 = 60;
+        int a2 = 0b111100;  //представили число 60 в двоичной системе  binary
+        int a3 = 074; //восмеричная система Octal
+        int a4 = 0x3c; // в Hecadecimal
+
+        int a5 = 1_000_000;  // более читабельно выглядит ! для удобства
+
+
+        System.out.println(a4);
+
+    }
 }
